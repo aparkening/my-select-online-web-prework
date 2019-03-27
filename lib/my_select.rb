c05e609aa52da69be70b452a4d7db7fbe8cea273
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:47:15 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,6 @@
-# - .select returns new collection for which block's condition is true
+# Returns new array for which block's condition is true
 def my_select(collection)
- # your code here!
+
+
+
 end

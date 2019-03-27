ce50a4ed2dfdb7435db8b6ed155dfabd788dd6aa
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:57:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,24 +10,3 @@ def my_select(collection)
   end
   new_collection                   # Return modified array
 end
-
-
-
-  it "yields the correct element" do
-    my_select(nums) do |number|
-      expect(number).to_not eq(nil)
-    end
-  end
-
-  it "selects the items in the collection to which the block returns true" do
-    expect(my_select(nums) do |num|
-      num.even?
-    end).to eq([2, 4])
-  end
-
-  it 'does not modify the original collection' do
-    my_select(nums) do |num|
-      num.even?
-    end
-    expect(nums).to eq([1, 2, 3, 4, 5])
-  end

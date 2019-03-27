8e248dbf1c888f2c8bac54320f735d07b3bcd0b7
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:56:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   i = 0
   while i < collection.length
     if yield(collection[i])
-      new_collection << yield(collection[i])
+      new_collection << collection[i]
     end
     i += 1
   end
